4b78e2dc7f907173fdaa165d690d25197d85efc6
syuuya-nakatomi <m21107@g.metro-cit.ac.jp>
2023-03-08 14:26:29 +0900

    foundation v6

--- cmd/moff/main.go
+++ cmd/moff/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 
 	"github.com/nippati/moff/pkg/playbook"
@@ -12,7 +14,6 @@ import (
 	"github.com/nippati/moff/pkg/ui"
 )
 
-// hoge
 func main() {
 	// Read the JSON file of vuls scan results
 	file, err := os.Open("vuls-results.json")
@@ -27,14 +28,18 @@ func main() {
 		log.Fatalf("error decoding JSON: %v", err)
 	}
 
-	// Convert []string to []int
+	// Extract vulnerability IDs
+	re := regexp.MustCompile(`CVE-(\d{4})-(\d+)`)
 	var vulns []int
 	for _, v := range results.Vulnerabilities {
-		id, err := strconv.Atoi(v)
-		if err != nil {
-			log.Fatalf("error converting vulnerability ID to int: %v", err)
+		matches := re.FindStringSubmatch(v)
+		if len(matches) == 3 {
+			id, err := strconv.Atoi(matches[2])
+			if err != nil {
+				log.Fatalf("error converting vulnerability ID to int: %v", err)
+			}
+			vulns = append(vulns, id)
 		}
-		vulns = append(vulns, id)
 	}
 
 	// Convert []int to []ui.Vulnerability
@@ -43,16 +48,31 @@ func main() {
 		uiVulns = append(uiVulns, ui.Vulnerability{ID: v})
 	}
 
-	// Display the list of vulnerabilities and allow the user to select which ones to deploy
-	selected, err := ui.SelectVulnerabilities(uiVulns)
-	if err != nil {
-		fmt.Println("Error selecting vulnerabilities:", err)
-		return
-	}
+	// Create a HTTP server and listen for requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Display the list of vulnerabilities and allow the user to select which ones to deploy
+		selected, err := ui.SelectVulnerabilities(uiVulns)
+		if err != nil {
+			fmt.Println("Error selecting vulnerabilities:", err)
+			return
+		}
+		// Convert []int to []string
+		var selectedStr []string
+		for _, v := range selected {
+			selectedStr = append(selectedStr, strconv.Itoa(v))
+		}
 
-	// Generate an Ansible playbook based on the selected vulnerabilities
-	playbook := playbook.Generate(selected)
+		// Generate an Ansible playbook based on the selected vulnerabilities
+		playbook := playbook.Generate(selectedStr)
 
-	// Print the playbook to the console
-	fmt.Println(playbook)
+		// Generate an Ansible playbook based on the selected vulnerabilities
+
+		// Print the playbook to the console
+		fmt.Println(playbook)
+
+		// Return the playbook to the user
+		w.Write([]byte(playbook))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -1,68 +1,130 @@
 package ui
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Vulnerability struct {
-	ID          int
-	Title       string
-	Description string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-func SelectVulnerabilities(vulns []Vulnerability) ([]string, error) {
-	selected := make(map[string]bool)
+func SelectVulnerabilities(vulns []Vulnerability) ([]int, error) {
+	var selected []int
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			tmpl, err := template.ParseFiles("templates/index.html")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			err = tmpl.Execute(w, vulns)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			return
+	// Create a map of vulnerabilities for easy lookups
+	vulnMap := make(map[int]Vulnerability)
+	for _, vuln := range vulns {
+		vulnMap[vuln.ID] = vuln
+	}
+
+	// Prompt the user to select vulnerabilities to deploy
+	for {
+		// Print the list of available vulnerabilities
+		for _, vuln := range vulns {
+			fmt.Printf("[%d] %s\n", vuln.ID, vuln.Name)
 		}
 
-		err := r.ParseForm()
+		// Prompt the user to select a vulnerability
+		fmt.Print("Select a vulnerability to deploy (0 to finish): ")
+		var input string
+		fmt.Scanln(&input)
+
+		// Convert the input to an integer
+		id, err := strconv.Atoi(input)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			fmt.Println("Invalid input")
+			continue
 		}
 
-		for k := range r.PostForm {
-			selected[k] = true
+		// Check if the user wants to finish selecting vulnerabilities
+		if id == 0 {
+			break
 		}
 
-		tmpl, err := template.ParseFiles("templates/result.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		// Check if the selected vulnerability exists
+		if _, ok := vulnMap[id]; !ok {
+			fmt.Println("Invalid selection")
+			continue
 		}
-		err = tmpl.Execute(w, vulns)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
 
-	fmt.Println("Server started at http://localhost:8080")
-	fmt.Println("Press CTRL-C to stop")
+		// Add the selected vulnerability to the list
+		selected = append(selected, id)
+	}
 
-	err := http.ListenAndServe(":8080", nil)
+	return selected, nil
+}
+
+func LoadVulnerabilities(filePath string) ([]Vulnerability, error) {
+	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	var selectedSlice []string
-	for v := range selected {
-		selectedSlice = append(selectedSlice, v)
+	var vulns []Vulnerability
+	err = json.Unmarshal(data, &vulns)
+	if err != nil {
+		return nil, err
 	}
-	return selectedSlice, nil
+
+	return vulns, nil
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	vulns, err := LoadVulnerabilities("vuls-results.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, vulns)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func SelectedHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	selectedIDs := r.Form["vulns"]
+	var selected []Vulnerability
+	for _, idStr := range selectedIDs {
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		selected = append(selected, Vulnerability{ID: id})
+	}
+
+	playbook := Generate(selected)
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", "attachment; filename=ansible-playbook.yml")
+	w.Write([]byte(playbook))
+}
+
+func Generate(selected []Vulnerability) string {
+	// Generate the Ansible playbook
+	var playbook strings.Builder
+	playbook.WriteString("hosts: all\n")
+	playbook.WriteString("tasks:\n")
+	for _, vuln := range selected {
+		playbook.WriteString(fmt.Sprintf("- name: Deploy %s\n", vuln.Name))
+		playbook.WriteString("  # TODO: Add deployment steps here\n")
+		playbook.WriteString(fmt.Sprintf("  shell: echo 'Deploying %s'\n", vuln.Name))
+	}
+	return playbook.String()
 }
