5df1da62d369c772fb6ed823c352d3c656d4ac91
syuuya-nakatomi <m21107@g.metro-cit.ac.jp>
2023-03-08 20:44:18 +0900

    develop/v10

--- cmd/moff/main.go
+++ cmd/moff/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
 	"log"
-	"net/http"
 	"os"
-	"os/exec"
 
 	"github.com/nippati/moff/pkg/scan"
 	"github.com/nippati/moff/pkg/ui"
@@ -35,112 +32,23 @@ func main() {
 	var vulns []ui.Vulnerability
 	for _, r := range results.Vulnerabilities {
 		v := ui.Vulnerability{
-			ID: r.cve, // Use VulnID instead of ID
+			ID: r.VulnID,
 		}
 		vulns = append(vulns, v)
 	}
 
-	// Define the HTML template for the vulnerabilities page
-	tmpl := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Vulnerabilities</title>
-		</head>
-		<body>
-			<h1>Select Vulnerabilities</h1>
-			<form action="/" method="POST">
-				{{range .}}
-				<input type="checkbox" name="vulns" value="{{.ID}}">{{.ID}}<br>
-				{{end}}
-				<input type="submit" value="Generate Ansible Playbook">
-			</form>
-		</body>
-		</html>
-	`
-	// Parse the template
-	t, err := template.New("vulns").Parse(tmpl)
+	// Render the template with the vulnerability data
+	tmpl, err := template.ParseFiles("template.html")
 	if err != nil {
 		log.Fatalf("error parsing template: %v", err)
 	}
-
-	// Create a HTTP server and listen for requests
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			// User submitted the form, process the selected vulnerabilities
-			r.ParseForm()
-			selectedIDs := r.Form["vulns"]
-
-			var selected []ui.Vulnerability
-			for _, v := range vulns {
-				for _, id := range selectedIDs {
-					if v.ID == id {
-						selected = append(selected, v)
-					}
-				}
-			}
-
-			if len(selected) == 0 {
-				fmt.Fprintln(w, "No vulnerabilities selected!")
-				return
-			}
-
-			playbook := GeneratePlaybook(selected)
-
-			// Write the playbook to a file
-			file, err := os.Create("playbook.yml")
-			if err != nil {
-				fmt.Fprintln(w, "Error creating playbook file:", err)
-				return
-			}
-			defer file.Close()
-
-			_, err = file.WriteString(playbook)
-			if err != nil {
-				fmt.Fprintln(w, "Error writing playbook file:", err)
-				return
-			}
-
-			// Return a success message to the user
-			w.Write([]byte("Ansible playbook successfully generated!"))
-
-			// Execute the Ansible playbook
-			err = RunPlaybook("playbook.yml")
-			if err != nil {
-				fmt.Println("Error executing playbook:", err)
-				return
-			}
-
-			// Return a success message to the user
-			w.Write([]byte("Ansible playbook successfully executed!"))
-		} else {
-			// User requested the vulnerabilities page
-			err := t.Execute(w, vulns)
-			if err != nil {
-				fmt.Fprintln(w, "Error executing template:", err)
-				return
-			}
-		}
-
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-// GeneratePlaybook generates an Ansible playbook based on the selected vulnerabilities
-func GeneratePlaybook(vulns []ui.Vulnerability) string {
-	// TODO: implement playbook generation logic
-	return ""
-}
-
-// RunPlaybook runs the specified Ansible playbook
-func RunPlaybook(playbook string) error {
-	// Execute the Ansible playbook using the "ansible-playbook" command
-	cmd := exec.Command("ansible-playbook", playbook)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error executing Ansible playbook: %v", err)
+	data := struct {
+		Vulns []ui.Vulnerability
+	}{
+		Vulns: vulns,
 	}
-	return nil
+	err = tmpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalf("error rendering template: %v", err)
+	}
 }
